63b762cf22e8a5e82c8ba72f76b66ac84f2df283
Victor Dângelo <terrasterd@gmail.com>
2020-07-01 13:40:04 -0300

    Alterações na dela de monitorar paciente

--- script/MonitoringScreen.js
+++ script/MonitoringScreen.js
@@ -4,20 +4,38 @@ function randomNumber(min, max){
 
 }
 
-$(function(){
-			$.ajax({
-				 url: 'MonitoringScreen.html', // página da requisição externa
-				 type: 'GET',
+function validate(min, max, defaultValue, value){
 
-				 // parâmetro "html" vem com o conteúdo da página completo
-				 success: function(html) {
+    value = randomNumber(min, max);
+    if(value > defaultValue+2 || value < defaultValue-2)
+        return false;
+    else
+        return true;
 
-					 var nomePaciente = $(html).find('h4');
-					 $("#nomePaciente").html(nomePaciente); // Pega  o nome do paciente na pagina de pacientes
+}
+function refreshData(){
+    document.getElementById("pressaoSistolica").innerHTML = 0;
+    $(document).ready(function(){
+        setInterval(function(){
+            var validationPressaoSis = validate(document.getElementById("PressãoPadraoSistolica").value-3, 
+                document.getElementById("PressãoPadraoSistolica").value+3,
+                document.getElementById("PressãoPadraoSistolica").value,
+                document.getElementById("pressaoSistolica").value);
+            var validationPressaoDia = validate(document.getElementById("PressãoPadraoDiastolica").innerHTML-3, 
+                document.getElementById("PressãoPadraoDiastolica").innerHTML+3,
+                document.getElementById("PressãoPadraoDiastolica").innerHTML,
+                document.getElementById("pressaoDiastolica").innerHTML);
+            var validationTemp = validate(document.getElementById("TemperaturaPadrao").innerHTML-3, 
+                document.getElementById("TemperaturaPadrao").innerHTML+3,
+                document.getElementById("TemperaturaPadrao").innerHTML,
+                document.getElementById("Temperatura").innerHTML)+"°C";
+            $("#pressaoSistolica").load(window.location.href + "#pressaoSistolica");
+            $("#pressaoDiastolica").load(window.location.href + "#pressaoDiastolica");
+            $("#Temperatura").load(window.location.href + "#Temperatura");
+            if(!validationPressaoSis || !validationPressaoDia || !validationTemp){
+                //Chama pop-up 
+            }
+        }, 3000);
+    });
+}
 
-					 var dadosPaciente = $(html).find('h5'); // Pega o nome do paciente na pagina de pacientes
- 					$("#dadosPaciente").html(dadosPaciente);
-
-			 }
-		 });
-	 });
