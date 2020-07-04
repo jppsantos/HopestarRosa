b8ec8225d5b474b73e68d39785183b9a656bd20b
Gabriel Ferraz Fontes <brielff@hotmail.com>
2020-07-04 16:23:36 -0300

    transições entre telas quase terminado

--- script/verifyLogin.js
+++ script/verifyLogin.js
@@ -1,10 +1,15 @@
+/**
+ * verify if login and password matches, if matches, send to patients page, if not, send a alert
+ */
 function verifyLogin(){
+    /// user email
     let usuario = document.getElementById('txtEmail').value;
+    /// user password
     let senha = document.getElementById('senha').value;
 
     if (usuario.localeCompare('resp@hopstar.com.br') == 0 && senha.localeCompare('12345') == 0){
-        alert('yay');
+        window.location.href = "../html/PatientsList.htm";
     } else {
-        alert('nay');
+        alert('Usuario ou senha incorreto!');
     }
 }
