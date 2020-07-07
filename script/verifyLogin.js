26e2c114a14c8a4c91d25ef15a6de82325cfec61
Joao Paulo <joao.auhsey@gmail.com>
2020-07-07 12:26:30 -0300

    Comunicar com API o Login e Cadastrar Paciente

--- script/verifyLogin.js
+++ script/verifyLogin.js
@@ -1,7 +1,7 @@
 /**
  * verify if login and password matches, if matches, send to patients page, if not, send a alert
  */
-function verifyLogin(){
+function verifyLogi(){
     /// user email
     let usuario = document.getElementById('txtEmail').value;
     /// user password
@@ -12,4 +12,40 @@ function verifyLogin(){
     } else {
         alert('Usuario ou senha incorreto!');
     }
+}
+
+/**
+ * verify if login and password matches, if matches, send to patients page, if not, send a alert
+ */
+function verifyLogin(){
+    // base url
+    const url = "https://vapor-projeto-hopstar.herokuapp.com/api/login";
+    /// user email
+    let email = document.getElementById('email').value;
+    /// user password
+    let password = document.getElementById('password').value;
+
+    // var auth = "Basic " + btoa(user + ":" + password);
+    // `Basic ${btoa(`${user}:${password}`)}`
+
+    const parameters = {
+        headers: {
+            "Authorization" : `Basic ${btoa(`${email}:${password}`)}`
+        },
+        method: "POST"
+    };
+
+    fetch(url,parameters)
+    // .then(resp => resp)
+    .then (r => r.json().then(data => ({status: r.status, body: data})))
+    .then(data => {
+         if (data.status == 200) {
+            // var json = await response.json();
+            sessionStorage.setItem("idManager", data.body.id);
+            window.location.href = "../html/PatientsList.htm";
+        }
+    })
+    .catch (error => {
+        alert("Login inválido, Tente novamente 😉")
+    })
 }
