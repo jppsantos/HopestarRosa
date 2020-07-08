dae00344176195724a8dc9ef755333457247e7cc
Lucas <lucashpc91@gmail.com>
2020-07-08 01:02:25 -0300

    Nome duplicado.

--- manager/PatientManager.js
+++ manager/PatientManager.js
@@ -67,7 +67,6 @@ function createPatientList(patients) {
 
         var nameView = document.createElement('H5')
         nameView.className = "card-text name"
-        nameView.innerHTML += patient.name
 
         card.appendChild(nameView)
         var name = patient.name
@@ -136,62 +135,3 @@ function filterPatient() {
         }
     }
 }
-
-
-
-
-// quick search regex
-var qsRegex;
-var buttonFilter;
-
-// init Isotope
-var $grid = $('.container').isotope({
-  itemSelector: '.card',
-  layoutMode: 'fitRows',
-  filter: function() {
-    var $this = $(this);
-    var searchResult = qsRegex ? $this.text().match( qsRegex ) : true;
-    var buttonResult = buttonFilter ? $this.is( buttonFilter ) : true;
-    return searchResult && buttonResult;
-  }
-});
-
-$('#filters').on( 'click', 'button', function() {
-  buttonFilter = $( this ).attr('data-filter');
-  $grid.isotope();
-});
-
-// use value of search field to filter
-var $quicksearch = $('#quicksearch').keyup( debounce( function() {
-  qsRegex = new RegExp( $quicksearch.val(), 'gi' );
-  $grid.isotope();
-}) );
-
-
-  // change is-checked class on buttons
-$('.btn-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
-  });
-});
-  
-
-// debounce so filtering doesn't happen every millisecond
-function debounce( fn, threshold ) {
-  var timeout;
-  return function debounced() {
-    if ( timeout ) {
-      clearTimeout( timeout );
-    }
-    function delayed() {
-      fn();
-      timeout = null;
-    }
-    setTimeout( delayed, threshold || 100 );
-  };
-}
-
-
-
